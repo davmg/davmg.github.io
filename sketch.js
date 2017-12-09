96b3204d095c78ee8ed5c99318e6357b8e4935a5
David Martínez Gordillo <31197495+davmg@users.noreply.github.com>
2017-12-09 00:22:50 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,22 +3,59 @@ var h = window.innerHeight;
 var canvas;
 var marca1;
 var marca2;
+var flecha1;
+var flecha2;
+var yflecha;
+var subiendo;
+var vel;
+var diam; 
 
 function preload() {
 	marca1 = loadImage("data/marca1.png");
 	marca2 = loadImage("data/marca2.png");
+	flecha1 = loadImage("data/flecha1.png");
+	flecha2 = loadImage("data/flecha2.png");
 }
 
 function setup() {
 	canvas = createCanvas(w, h);
 	canvas.parent("canvas-container");
 	imageMode(CENTER);
+	yflecha=h-40;
+	subiendo=true;
+	vel=1;
+	diam=0;
 }
 
 function draw() {
 	background(255);
-
+	if(subiendo){
+		yflecha-=vel;
+		if(yflecha<height-50){
+			subiendo=false;	
+		} 
+	}else{
+		yflecha+=vel;
+		if(yflecha>height-40){
+			subiendo=true;	
+		} 
+	}
+	if(dist(mouseX,mouseY,width/2,height-45)<20){
+		if(diam<80){
+			var v = map(diam, 0, 80, 50, 0);
+			diam+=v;
+		}
+	}else{
+		if(diam>0){
+			var vv = map(diam, 80, 0, 50, 2);
+			diam-=vv;
+		}
+	}
+		fill(230);
+		noStroke();
+		ellipse(width/2, yflecha-2,diam,diam);
 	image(marca2, w/2, height/2, marca1.width/3, marca1.height/3);
+	image(flecha2, w/2, yflecha, flecha2.width/1.5, flecha2.height/1.5);
 	var t=0;
 	if (mouseX<=w*0.4) {
 		t = map(mouseX, w*0.1, w*0.4, 255, 0);
@@ -29,11 +66,20 @@ function draw() {
 	rect(0, 0, w, h);
 	tint(255,t);
 	image(marca1, w/2, h/2, marca1.width/3, marca1.height/3);
+	image(flecha1, w/2, yflecha, flecha2.width/1.5, flecha2.height/1.5);
 	noTint();
 }
 
 function mousePressed(){
-	
+	if(dist(mouseX,mouseY,width/2,height-45)<20){
+		var $target = $('#david');
+		$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+		if ($target.length) {
+			var targetOffset = $target.offset().top;
+			$('html,body').animate({scrollTop: targetOffset}, 800);
+			return false;
+		}
+	}
 }
 
 function windowResized() {
